95139686e27bc3d743361e9e9d52ae39e29bf062
tison <wander4096@gmail.com>
2022-01-09 14:22:23 +0800

    kernel: in favor of UpdateReader
    Signed-off-by: tison <wander4096@gmail.com>

--- src/kernel/src/local/kernel.rs
+++ src/kernel/src/local/kernel.rs
@@ -19,7 +19,7 @@ use engula_storage::Storage;
 use tokio::sync::broadcast;
 
 use super::{update_reader::UpdateReader, update_writer::UpdateWriter};
-use crate::{async_trait, KernelUpdate, Result, Sequence};
+use crate::{async_trait, KernelUpdate, Result, Sequence, UpdateReader as _};
 
 pub struct Kernel<J, S> {
     journal: Arc<J>,
@@ -28,30 +28,58 @@ pub struct Kernel<J, S> {
     update_tx: broadcast::Sender<(Sequence, KernelUpdate)>,
 }
 
-impl<J, S> Kernel<J, S> {
+impl<J, S> Kernel<J, S>
+where
+    J: Journal + Send + Sync + 'static,
+    S: Storage + Send + Sync + 'static,
+{
     pub async fn init(journal: J, storage: S) -> Result<Self> {
-        let (update_tx, _) = broadcast::channel(1024);
+        let (update_tx, update_rx) = broadcast::channel(1024);
+        let journal = Arc::new(journal);
+        let storage = Arc::new(storage);
+        let reader = UpdateReader::new(update_rx);
+        Self::subscribe_updates(reader, journal.clone(), storage.clone());
         Ok(Self {
-            journal: Arc::new(journal),
-            storage: Arc::new(storage),
+            journal,
+            storage,
             sequence: Arc::new(AtomicU64::new(0)),
             update_tx,
         })
+    }
+
+    fn subscribe_updates(mut reader: UpdateReader, journal: Arc<J>, storage: Arc<S>) {
+        tokio::spawn(async move {
+            loop {
+                let (_, update) = reader.wait_next().await.unwrap();
+                for stream in &update.add_streams {
+                    journal.create_stream(stream).await.unwrap();
+                }
+                for stream in &update.remove_streams {
+                    journal.delete_stream(stream).await.unwrap();
+                }
+                for bucket in &update.add_buckets {
+                    storage.create_bucket(bucket).await.unwrap();
+                }
+                for bucket in &update.remove_buckets {
+                    storage.delete_bucket(bucket).await.unwrap();
+                }
+            }
+        });
     }
 }
 
 #[async_trait]
 impl<J, S> crate::Kernel for Kernel<J, S>
 where
-    J: Journal + Send + Sync,
-    S: Storage + Send + Sync,
+    J: Journal + Send + Sync + 'static,
+    S: Storage + Send + Sync + 'static,
 {
     type RandomReader = S::RandomReader;
     type SequentialWriter = S::SequentialWriter;
     type StreamReader = J::StreamReader;
     type StreamWriter = J::StreamWriter;
     type UpdateReader = UpdateReader;
-    type UpdateWriter = UpdateWriter<J, S>;
+    type UpdateWriter = UpdateWriter;
 
     async fn new_update_reader(&self) -> Result<Self::UpdateReader> {
         let reader = UpdateReader::new(self.update_tx.subscribe());
@@ -59,12 +87,7 @@ where
     }
 
     async fn new_update_writer(&self) -> Result<Self::UpdateWriter> {
-        let writer = UpdateWriter::new(
-            self.journal.clone(),
-            self.storage.clone(),
-            self.sequence.clone(),
-            self.update_tx.clone(),
-        );
+        let writer = UpdateWriter::new(self.sequence.clone(), self.update_tx.clone());
         Ok(writer)
     }
 
